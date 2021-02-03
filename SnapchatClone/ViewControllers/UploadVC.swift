a8564097e7bb02f9aece58af4cacae5844ec32d1
onurbasdas <onurebasdase@hotmail.com>
2021-02-03 03:15:03 +0300

    add upload files completed

--- SnapchatClone/ViewControllers/UploadVC.swift
+++ SnapchatClone/ViewControllers/UploadVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class UploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -34,6 +35,40 @@ class UploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationC
 
    
     @IBAction func uploadClicked(_ sender: Any) {
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
+        let mediaFolder = storageReference.child("media")
+        if let data = uploadImageView.image?.jpegData(compressionQuality: 0.5){
+            let uuid = UUID().uuidString
+            let imageReference = mediaFolder.child("\(uuid).jpg")
+            imageReference.putData(data, metadata: nil) { (metadata, error) in
+                if error != nil{
+                    self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
+                }else{
+                    imageReference.downloadURL { (url, error) in
+                        if error == nil{
+                            let imageUrl = url?.absoluteString
+                            let fireStore = Firestore.firestore()
+                            let snapDictionary = ["imageUrl" : imageUrl!,"snapOwner" : UserSingleton.sharedUserInfo.username,"date":FieldValue.serverTimestamp()] as [String : Any]
+                            fireStore.collection("Snaps").addDocument(data: snapDictionary) { (error) in
+                                if error != nil{
+                                    self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
+                                }else{
+                                    self.tabBarController?.selectedIndex = 0
+                                    self.uploadImageView.image = UIImage(named: "select.png")
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    func makeAlert(title: String,message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
